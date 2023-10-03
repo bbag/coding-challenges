27e2b92f9e4106d4a7bc62e43e7fdb10615c1556
bbag <bill.baggerman@gmail.com>
2023-10-03 00:09:14 -0400

    Started connect four challenge

--- src/challenges/connect-four/ConnectFour.tsx
+++ src/challenges/connect-four/ConnectFour.tsx
@@ -1,4 +1,60 @@
+import { useEffect, useState } from 'react'
+import styles from './_ConnectFour.module.css'
+
+type Board = (1 | 2 | null)[][]
+
+// Global constants for the game board
+const COLS = 7
+const ROWS = 6
+
 export default function ConnectFour() {
+    const [board, setBoard] = useState<Board>([])
+
+    // Create a blank game board array
+    useEffect(() => {
+        setBlankBoard()
+    }, [])
+
+    // Starts a fresh, empty new board
+    function setBlankBoard() {
+        let arr: Board = []
+        for (let i = 0; i < ROWS; i++) {
+            arr.push([])
+            for (let j = 0; j < COLS; j++) {
+                arr[i][j] = null
+            }
+        }
+        setBoard(arr)
+    }
+
+    // Handle adding a piece to the board when a column button is clicked
+    function addPiece(index: number) {
+
+        let lowestRowWithEmptySpace = -1
+
+        // Check each row to see if `index` has a piece on it already
+        for (let r = 0; r < ROWS; r++) {
+            if (board[r][index] === null) {
+                
+            }
+        }
+
+
+
+
+        setBoard((prevBoard) => {
+            // Clone the outer board array (i.e. the array of rows)
+            const newBoardArr = [...prevBoard]
+
+            // Clone the inner array that needs to be updated
+            const newRowArr = [...newBoardArr[index]]
+
+            newRowArr
+
+            return newBoardArr
+        })
+    }
+
     return (
         <>
             <h1>Connect Four</h1>
@@ -17,6 +73,29 @@ export default function ConnectFour() {
             </ol>
             <hr />
             {/* Add solution here */}
+            <div className={styles['connect-four']}>
+                <div className={styles['board']}>
+                    <div className={styles['top-buttons']}>
+                        {/* {Array.from(COLS).map((buttonCol, buttonColIndex) => (
+                            
+                        ))} */}
+                        {Array.from(Array(COLS)).map((buttonCol, buttonColIndex) => (
+                            <button key={buttonColIndex} onClick={() => addPiece(buttonColIndex)}>Cool</button>
+                        ))}
+                    </div>
+                    {board.map((row, rowIndex) => (
+                        <div className={styles['board-row']} key={rowIndex}>
+                            {row.map((col, colIndex) => (
+                                <div className={styles['board-col']} key={colIndex}>
+                                    <div className={styles['board-dot']}>
+                                        {col}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
